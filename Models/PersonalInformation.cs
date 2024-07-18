8682da2de0610aa7f9b708d00a5e1cb90572aae2
Formation <Formation@HNI-DN-VM2023>
2024-07-18 17:06:25 +0200

    test

---
+++ Models/PersonalInformation.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace TPLOCAL1.Models
+{
+    public class PersonalInformation
+    {
+        public int Id { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Name { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Forename { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Gender { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Adress { get; set; }
+
+        [StringLength(5)]
+        [Required]
+        public string? ZipCode { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? Town { get; set; }
+
+        [DataType(DataType.EmailAddress)]
+        public string? EmailAddress { get; set; }
+
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
+        public DateTime StartDateTraining { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? TypeTraining { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? CobolTraining { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        [Required]
+        public string? TrainingPurpose { get; set; }
+
+
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+    }
+}
